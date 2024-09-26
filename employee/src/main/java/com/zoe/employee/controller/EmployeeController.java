46392ec88b672345e1b87618d8028d95f578c42a
Zoe Z <167350133+zoesyz16@users.noreply.github.com>
2024-09-26 14:15:55 -0700

    complete CRUD features

---
+++ employee/src/main/java/com/zoe/employee/controller/EmployeeController.java
@@ -0,0 +1,54 @@
+package com.zoe.employee.controller;
+
+import com.zoe.employee.entity.Employee;
+import com.zoe.employee.service.EmployeeService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @PostMapping("/employee")
+    public Employee postEmployee(@RequestBody Employee employee) {
+        return employeeService.postEmployee(employee);
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+        try {
+            employeeService.deleteEmployee(id);
+            return new ResponseEntity<>("Employee with ID: " + id + " deleted successfully", HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<?> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeService.getEmployeeById(id);
+        if (employee == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(employee);
+    }
+
+    @PatchMapping("/employee/{id}")
+    public ResponseEntity<?> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        Employee udpateEmployee = employeeService.updateEmployee(id, employee);
+        if (udpateEmployee == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        return ResponseEntity.ok(udpateEmployee);
+    }
+}
---
+++ employee/src/main/java/com/zoe/employee/service/EmployeeService.java
@@ -0,0 +1,51 @@
+package com.zoe.employee.service;
+
+import com.zoe.employee.entity.Employee;
+import com.zoe.employee.repository.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public Employee postEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public void deleteEmployee(Long id) {
+        if (!employeeRepository.existsById(id)) {
+            throw new EntityNotFoundException("Employee with ID " + id + " not found");
+        }
+        employeeRepository.deleteById(id);
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public Employee updateEmployee(Long id, Employee employee) {
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+        if (optionalEmployee.isPresent()) {
+            Employee existingEmployee = optionalEmployee.get();
+
+            existingEmployee.setEmail(employee.getEmail());
+            existingEmployee.setName(employee.getName());
+            existingEmployee.setPhone(employee.getPhone());
+            existingEmployee.setDepartment(employee.getDepartment());
+
+            return employeeRepository.save(existingEmployee);
+        }
+        return null;
+    }
+}
